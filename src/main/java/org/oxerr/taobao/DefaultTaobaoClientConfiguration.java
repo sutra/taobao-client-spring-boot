eb283beaf3fa10be261e99387d48c0280e0f3a9e
Sutra Zhou <zhoushuqun@gmail.com>
2017-11-29 20:03:17 +0800

    Initial commit

---
+++ src/main/java/org/oxerr/taobao/DefaultTaobaoClientConfiguration.java
@@ -0,0 +1,46 @@
+package org.oxerr.taobao;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DefaultTaobaoClientConfiguration {
+
+	private final Logger log = Logger.getLogger(getClass().getName());
+
+	@Bean
+	public DefaultTaobaoClientProperties taobaoClientProperties() {
+		return new DefaultTaobaoClientProperties();
+	}
+
+	@Bean(name = "defaultTaobaoClient")
+	@Autowired
+	public Object taobaoClient(DefaultTaobaoClientProperties taobaoClientProperties) {
+		Object taobaoClient = null;
+		try {
+			taobaoClient = Class.forName("com.taobao.api.DefaultTaobaoClient")
+				.getConstructor(String.class, String.class, String.class,
+					String.class, int.class, int.class, String.class)
+				.newInstance(
+					taobaoClientProperties.getServerUrl(),
+					taobaoClientProperties.getAppKey(),
+					taobaoClientProperties.getAppSecret(),
+					taobaoClientProperties.getFormat(),
+					taobaoClientProperties.getConnectTimeout(),
+					taobaoClientProperties.getReadTimeout(),
+					taobaoClientProperties.getSignMethod()
+				);
+		} catch (ClassNotFoundException | NoSuchMethodException
+				| SecurityException | InstantiationException
+				| IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException e) {
+			log.severe(e.getMessage());
+		}
+		return taobaoClient;
+	}
+
+}
---
+++ src/main/java/org/oxerr/taobao/DefaultTaobaoClientProperties.java
@@ -0,0 +1,78 @@
+package org.oxerr.taobao;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @see com.taobao.api.DefaultTaobaoClient
+ */
+@ConfigurationProperties(prefix = DefaultTaobaoClientProperties.PREFIX)
+public class DefaultTaobaoClientProperties {
+
+	static final String PREFIX = "com.oxerr.taobao.client";
+
+	private String serverUrl;
+	private String appKey;
+	private String appSecret;
+	private String format = "json";
+	private String signMethod = "hmac";
+
+	private int connectTimeout = 15000;
+	private int readTimeout = 30000;
+
+	public String getServerUrl() {
+		return serverUrl;
+	}
+
+	public void setServerUrl(String serverUrl) {
+		this.serverUrl = serverUrl;
+	}
+
+	public String getAppKey() {
+		return appKey;
+	}
+
+	public void setAppKey(String appKey) {
+		this.appKey = appKey;
+	}
+
+	public String getAppSecret() {
+		return appSecret;
+	}
+
+	public void setAppSecret(String appSecret) {
+		this.appSecret = appSecret;
+	}
+
+	public String getFormat() {
+		return format;
+	}
+
+	public void setFormat(String format) {
+		this.format = format;
+	}
+
+	public String getSignMethod() {
+		return signMethod;
+	}
+
+	public void setSignMethod(String signMethod) {
+		this.signMethod = signMethod;
+	}
+
+	public int getConnectTimeout() {
+		return connectTimeout;
+	}
+
+	public void setConnectTimeout(int connectTimeout) {
+		this.connectTimeout = connectTimeout;
+	}
+
+	public int getReadTimeout() {
+		return readTimeout;
+	}
+
+	public void setReadTimeout(int readTimeout) {
+		this.readTimeout = readTimeout;
+	}
+
+}
